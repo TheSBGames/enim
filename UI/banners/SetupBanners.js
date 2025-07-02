81e0163f00341c87eda7d2a7e459abfb314e69e9
TheSBGames <thesdgams07@gmail.com>
2025-07-02 21:13:35 +0530

    Update SetupBanners.js

--- UI/banners/SetupBanners.js
+++ UI/banners/SetupBanners.js
@@ -1,5 +1,5 @@
 const setupBanners = {
-    helpBanner : 'https://images.app.goo.gl/sLNPncALtfvFahyq7',
+    helpBanner : 'https://ibb.co/8LwsF3LG',
     verificationBanner : 'https://i.ibb.co/pjHDXzXx/static.png',
     ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
     autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
