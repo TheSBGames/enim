d01541459234e753b069cb3e9c760c0431309673
TheSBGames <thesdgams07@gmail.com>
2025-07-02 19:35:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
       { name: 'TFARC_ENIM', type: ActivityType.Streaming, url: 'https://youtube.com/@TFARC_ENIM' },
       { name: 'TFARC_ENIM', type: ActivityType.Listening },
     ],
-    songStatus: true
+    songStatus: false
   },
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
